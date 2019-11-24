1fad621092a00636d7fb9246d88e95e6d49ceee2
Jackson Felty <jfelty@e1z2r6p11.42.us.org>
2019-11-23 19:42:10 -0800

    basit working mandlebrot fractal with zoom

--- fractol.c
+++ fractol.c
@@ -6,11 +6,13 @@
 /*   By: jfelty <jfelty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 00:23:22 by jfelty            #+#    #+#             */
-/*   Updated: 2019/11/13 15:50:08 by jfelty           ###   ########.fr       */
+/*   Updated: 2019/11/23 19:41:07 by jfelty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./fractol.h"
+
+void	reset_pos(t_pos *pos, t_frac *frac);
 
 void	img_pixel_put(t_img *img, int x, int y, int color)
 {
@@ -18,14 +20,84 @@ void	img_pixel_put(t_img *img, int x, int y, int color)
 		*(int*)(img->data + ((x + y * WINX) * img->bpp)) = color;
 }
 
+void		view_mandlebrot(t_frac *frac, t_wndw *wndw)
+{
+	mlx_clear_window(wndw->mlx, wndw->win);
+	frac->px = 0;
+	frac->py = 0;
+
+	while (frac->px < WINX && frac->py < WINY)
+	{
+		frac->wx = 0;
+		frac->wy = 0;
+		frac->p = 0;
+		frac->fn = frac->px / wndw->pos->zoom + wndw->pos->fx;
+		frac->fi = frac->py / wndw->pos->zoom + wndw->pos->fy;
+		// frac->fn = frac->px / ((WINX / wndw->pos->zoom) - frac->fx) - (wndw->pos->zoom / 3 * 2);
+		// frac->fi = frac->py / ((WINY / wndw->pos->zoom) - frac->fy) - (wndw->pos->zoom / 2);
+
+		while (frac->wx * frac->wx + frac->wy * frac->wy < 4 && frac->p < frac->p_max)
+		{
+			frac->tmp = frac->wx * frac->wx - frac->wy * frac->wy + frac->fn;
+			frac->wy = 2 * frac->wx * frac->wy + frac->fi;
+			frac->wx = frac->tmp;
+			frac->p++;
+		}
+
+		if (frac->p == frac->p_max)
+			img_pixel_put(wndw->img, frac->px, frac->py, 0xFFFFFF);
+		else
+			img_pixel_put(wndw->img, frac->px, frac->py, 0x088000 - frac->p * frac->p_max);
+		if (++frac->px == WINX)
+		{
+			frac->px = 0;
+			frac->py++;
+		}
+	}
+	mlx_put_image_to_window(wndw->mlx, wndw->win, wndw->img->image, 0, 0);
+}
+
+void		zoom(int in, t_pos *pos)
+{
+	if (in)
+	{
+		pos->fx = (pos->mouse_x / pos->zoom + pos->fx) - (pos->mouse_x / (pos->zoom * ZOOM));
+		pos->fy = (pos->mouse_y / pos->zoom + pos->fy) - (pos->mouse_y / (pos->zoom * ZOOM));
+		pos->zoom *= ZOOM;
+	}
+	else
+	{
+		pos->fx = (pos->mouse_x / pos->zoom + pos->fx) - (pos->mouse_x / (pos->zoom / ZOOM));
+		pos->fy = (pos->mouse_y / pos->zoom + pos->fy) - (pos->mouse_y / (pos->zoom / ZOOM));
+		pos->zoom /= ZOOM;
+	}
+}
+
 int			hook_key(int key, t_wndw *wndw)
 {
 	ft_putnbr(key);
 	ft_putchar(' ');
-	if (key == 53)
+	if (key == 24 || (key == 27 && wndw->frac->p_max > 5))
+		wndw->frac->p_max += (key == 24) ? 5 : -5;
+	else if (key == 30 || (key == 33 && wndw->frac->p_max > 1))
+		wndw->frac->p_max += (key == 30) ? 1 : -1;
+	else if (key == 15)
+		reset_pos(wndw->pos, wndw->frac);
+	else if (key == 53)
 		exit(1);
-	if (wndw->frac->tmp)
-		wndw->frac->tmp = 0;
+	ft_printf("%d\n", wndw->frac->p_max);
+	view_mandlebrot(wndw->frac, wndw);
+	return (0);
+}
+
+int			hook_mouse(int button, int x, int y, t_wndw *wndw)
+{
+	ft_printf("%d@x%dy%d ", button, x, y);
+	wndw->pos->mouse_x = x;
+	wndw->pos->mouse_y = y;
+	if (button == 4 || button == 1 || button == 5 || button == 2)
+		zoom (button == 4 || button == 1 ? 0 : 1, wndw->pos);
+	view_mandlebrot(wndw->frac, wndw);
 	return (0);
 }
 
@@ -43,6 +115,16 @@ t_img	*new_im(t_wndw *wndw)
 	return (image);
 }
 
+void	reset_pos(t_pos *pos, t_frac *frac)
+{
+	pos->zoom = WINY / 4;
+	pos->mouse_x = 0;
+	pos->mouse_y = 0;
+	pos->fx = -2.0f;
+	pos->fy = -2.0f;
+	frac->p_max = 30;
+}
+
 t_wndw		*initialize_wndw()
 {
 	t_wndw	*wndw;
@@ -55,48 +137,13 @@ t_wndw		*initialize_wndw()
 		return (NULL);
 	if (!(wndw->frac = ft_memalloc(sizeof(t_frac))))
 		return (NULL);
+	if (!(wndw->pos = ft_memalloc(sizeof(t_pos))))
+		return (NULL);
+	reset_pos(wndw->pos, wndw->frac);
 	wndw->img = new_im(wndw);
 	mlx_key_hook(wndw->win, hook_key, wndw);
+	mlx_mouse_hook(wndw->win, hook_mouse, wndw);
 	return (wndw);
-}
-
-void		view_mandlebrot(t_frac *frac, t_wndw *wndw)
-{
-	frac->px = 0;
-	frac->py = 0;
-	frac->p_max = 500;
-
-	while (frac->px < WINX && frac->py < WINY)
-	{
-		frac->fx = -2.0f;
-		frac->fy = -2.0f;
-		frac->wx = 0;
-		frac->wy = 0;
-		frac->p = 0;
-		frac->fn = frac->px / (500 + frac->fx);
-		frac->fi = frac->py / (500 + frac->fy);
-
-		while (frac->wx * frac->wx + frac->wy * frac->wy < 4 && frac->p < frac->p_max)
-		{
-			frac->tmp = frac->wx * frac->wx - frac->wy * frac->wy + frac->fn;
-			frac->wy = 2 * frac->wx * frac->wy + frac->fi;
-			frac->wx = frac->tmp;
-			frac->p++;
-		}
-
-		if (frac->p == frac->p_max)
-			img_pixel_put(wndw->img, frac->px, frac->py, 0xFFFFFF);
-		else
-			img_pixel_put(wndw->img, frac->px, frac->py, (0x008080 * frac->p));
-
-		frac->px++;
-		if (frac->px == WINX)
-		{
-			frac->px = 0;
-			frac->py++;
-		}
-	}
-	mlx_put_image_to_window(wndw->mlx, wndw->win, wndw->img->image, 0, 0);
 }
 
 int			main()
@@ -107,7 +154,6 @@ int			main()
 	
 	view_mandlebrot(wndw->frac, wndw);
 
-	// img_pixel_put(wndw->img, 0, 0, 0xFFFFFF);
 	// mlx_put_image_to_window(wndw->mlx, wndw->win, wndw->img->image, 0, 0);
 	mlx_loop(wndw->mlx);
 	return (0);
--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: jfelty <jfelty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/10 18:07:42 by jfelty            #+#    #+#             */
-/*   Updated: 2019/11/13 15:09:06 by jfelty           ###   ########.fr       */
+/*   Updated: 2019/11/23 19:41:14 by jfelty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 # include <math.h>
 # include <unistd.h>
 
-# define WINX 1000
-# define WINY 1000
+# define WINX 500
+# define WINY 500
 # define MAXCOLOR 16777215
+# define ZOOM 2.0
 
 /*
 **	px = pixel x, x on the screen
@@ -38,12 +39,19 @@ typedef struct      s_frac
 	int				p_max;
 	double			wx;
 	double			wy;
-    double          fx;
-    double          fy;
     double          fn;
     double          fi;
     double          tmp;
 }                   t_frac;
+
+typedef struct		s_pos
+{
+	double			zoom;
+    double          fx;
+    double          fy;
+	int				mouse_x;
+	int				mouse_y;
+}					t_pos;
 
 typedef struct		s_img
 {
@@ -59,6 +67,7 @@ typedef struct		s_wndw
 	void			*win;
 	t_img			*img;
     t_frac			*frac;
+	t_pos			*pos;
 }					t_wndw;
 
 #endif
